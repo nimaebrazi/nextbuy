cad69f9cb34eb32df4704a4588724f4e64b4f6f2
Nima Ebrazi <n.ebrazi@sheypoor.com>
2026-08-02 22:58:23 +0330

    feat(passport): compatible json mapper with 4.1.0 version.

--- passport/src/test/java/com/nextbuy/passport/support/controller/MockMvcUtils.java
+++ passport/src/test/java/com/nextbuy/passport/support/controller/MockMvcUtils.java
@@ -1,18 +1,20 @@
 package com.nextbuy.passport.support.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import tools.jackson.databind.json.JsonMapper;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 public class MockMvcUtils {
     private final MockMvc mockMvc;
-    private final ObjectMapper objectMapper;
+    private final JsonMapper objectMapper;
 
-    public MockMvcUtils(MockMvc mockMvc, ObjectMapper objectMapper) {
+    public MockMvcUtils(MockMvc mockMvc, JsonMapper objectMapper) {
         this.mockMvc = mockMvc;
         this.objectMapper = objectMapper;
     }
