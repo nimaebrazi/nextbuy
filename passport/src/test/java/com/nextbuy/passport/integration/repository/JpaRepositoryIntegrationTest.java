ea386654d1413a36c84a3ce7f479ea3f9c3b5fd2
Nima Ebrazi <n.ebrazi@sheypoor.com>
2026-08-02 22:57:50 +0330

    feat(passport): compatible flyway with 4.1.0 version.

--- passport/src/test/java/com/nextbuy/passport/integration/repository/JpaRepositoryIntegrationTest.java
+++ passport/src/test/java/com/nextbuy/passport/integration/repository/JpaRepositoryIntegrationTest.java
@@ -2,14 +2,17 @@ package com.nextbuy.passport.integration.repository;
 
 
 import com.nextbuy.passport.TestcontainersConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.flyway.autoconfigure.FlywayAutoConfiguration;
+import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
 @ActiveProfiles("test")
 @Import(TestcontainersConfiguration.class)
+@ImportAutoConfiguration(FlywayAutoConfiguration.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class JpaRepositoryIntegrationTest {
 }
